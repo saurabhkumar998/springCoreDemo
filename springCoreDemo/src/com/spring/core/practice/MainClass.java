3d57d3ba11200c58270905b7a67bccd2fee6e8a2
saurabhkumar998 <saurabh.kumar.2382000@gmail.com>
2022-12-29 06:23:55 +0530

    initialized git and committed files

---
+++ springCoreDemo/src/com/spring/core/practice/MainClass.java
@@ -0,0 +1,24 @@
+package com.spring.core.practice;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		//Triangle triangle = new Triangle();
+	
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		//Triangle triangle = (Triangle) factory.getBean("triangle");
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		
+		triangle.draw();
+		
+	}
+
+}
---
+++ springCoreDemo/src/com/spring/core/practice/Triangle.java
@@ -0,0 +1,9 @@
+package com.spring.core.practice;
+
+public class Triangle {
+	
+	void draw(){
+		System.out.println("Triangle Drawn");
+	}
+
+}
