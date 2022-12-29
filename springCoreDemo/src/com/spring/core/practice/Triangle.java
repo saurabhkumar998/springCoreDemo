ed63785847435b640dabd290cd10afe022894c57
saurabhkumar998 <saurabh.kumar.2382000@gmail.com>
2022-12-29 07:09:38 +0530

    initializing objects through xml

--- springCoreDemo/src/com/spring/core/practice/Triangle.java
+++ springCoreDemo/src/com/spring/core/practice/Triangle.java
@@ -1,9 +1,37 @@
 package com.spring.core.practice;
 
 public class Triangle {
+	String type;
+	int height;
 	
+	Triangle(String type){
+		this.type = type;
+	}
+	
+	Triangle(int height){
+		this.height = height;
+	}
+	
+	Triangle(String type, int height){
+		this.type = type;
+		this.height = height;
+	}
+	
+	
+	public int getHeight() {
+		return height;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+
 	void draw(){
-		System.out.println("Triangle Drawn");
+		System.out.println(getType() +" Triangle Drawn of height " + getHeight());
 	}
 
 }
