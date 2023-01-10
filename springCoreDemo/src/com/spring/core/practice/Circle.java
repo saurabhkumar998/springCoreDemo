17dfb377989d220b6e1e6c925c11b26308784e4f
saurabhkumar998 <saurabh.kumar.2382000@gmail.com>
2023-01-10 15:13:09 +0530

    using annotation-config instead of configuring each annotation bean

--- springCoreDemo/src/com/spring/core/practice/Circle.java
+++ springCoreDemo/src/com/spring/core/practice/Circle.java
@@ -1,6 +1,7 @@
 package com.spring.core.practice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Circle implements Shape{
 	
@@ -14,6 +15,7 @@ public class Circle implements Shape{
 
 
 	@Autowired
+	@Qualifier("circleRelated")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
